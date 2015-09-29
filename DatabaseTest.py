21f38fcf20acdd4ffde38cd2067fdebcbd8dca51
abhilash <abhi.r.j95@gmail.com>
2015-09-29 00:54:31 -0400

    id extraction done

--- DatabaseTest.py
+++ DatabaseTest.py
@@ -7,7 +7,7 @@ APIKEY = "ab22804642e54e6b8c943c70eb9eae09"
 conn=sqlite3.connect("fantasy.sqlite3")
 cur=conn.cursor()
 cur.execute("DROP TABLE FantasyLeague"	)
-cur.execute("CREATE TABLE FantasyLeague (id TEXT /*PRIMARY KEY NOT NULL*/,caption TEXT,league TEXT,year INTEGER,numberOfTeams INTEGER,numberOfGames INTEGER,lastUpdated TEXT)")
+cur.execute("CREATE TABLE FantasyLeague (id INTEGER PRIMARY KEY NOT NULL,caption TEXT,league TEXT,year INTEGER,numberOfTeams INTEGER,numberOfGames INTEGER,lastUpdated TEXT)")
 
 connection = http.client.HTTPConnection('api.football-data.org')
 headers = { 'X-Auth-Token': APIKEY }
@@ -15,7 +15,7 @@ connection.request('GET', '/alpha/soccerseasons', None, headers )
 response = json.loads(connection.getresponse().read().decode())
 
 for row in response:
-    tup=("Azeem is idiot",row['caption'],row['league'],row['year'],row['numberOfTeams'],row['numberOfGames'],row['lastUpdated'])
+    tup=(int(row['_links']['self']['href'].rsplit('/',1)[-1]),row['caption'],row['league'],row['year'],row['numberOfTeams'],row['numberOfGames'],row['lastUpdated'])
     cur.execute('INSERT INTO FantasyLeague VALUES(?,?,?,?,?,?,?)',tup)
     del tup
 conn.commit()
